cdaf787a263741c4bf30f36b52f3e14e9b34d03f
Eric Bischoff <ebischoff@suse.com>
2014-11-10 10:20:10 +0100

    Reorganizing layout of config files on reference machine Removing the infiniband hack

---
+++ basic-config-files/reference/cleanup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+for int_file in $(ls /etc/sysconfig/network/ifcfg-*); do
+  if [ -L $int_file ]; then
+    echo "Removing $int_file ..."
+    rm $int_file
+    rc=$?
+    $rc -eq 0 || exit $rc
+  else
+    echo "Skipping $int_file ..."
+  fi
+done
