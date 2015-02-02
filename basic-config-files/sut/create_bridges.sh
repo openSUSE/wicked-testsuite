804936cb6906547041220099dfc4a8100a573ad6
Bischoff <ebischoff@suse.com>
2015-02-02 20:56:38 +0100

    Script to create one hundred bridges

---
+++ basic-config-files/sut/create_bridges.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+for ((i = 0; i < 100; i++)); do
+  file=/etc/sysconfig/network/ifcfg-br$i
+  if [ ! -e $file ]; then
+    echo "BOOTPROTO=none"                >  $file
+    echo "BRIDGE=yes"                    >> $file
+    echo "BRIDGE_FORWARDDELAY=0"         >> $file
+    echo "BRIDGE_PORTS="                 >> $file
+    echo "BRIDGE_STP=off"                >> $file
+    echo "BROADCAST="                    >> $file
+    #echo "IPADDR=192.168.3.$i"          >> $file
+    echo "DHCLIENT_SET_DEFAULT_ROUTE=no" >> $file
+    echo "CHECK_DUPLICATE_IP=no"         >> $file
+    #echo "ETHTOOL_OPTIONS=''"           >> $file
+    #echo "MTU=''"                       >> $file
+    #echo "NETMASK=''"                   >> $file
+    #echo "NETWORK=''"                   >> $file
+    #echo "REMOTE_IPADDR=''"             >> $file
+    echo "STARTMODE=auto"                >> $file
+  fi
+done
