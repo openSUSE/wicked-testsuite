ccc156bab8780fd20d5575c5b5ce78b48635fe67
Bischoff <ebischoff@suse.com>
2015-02-18 15:39:18 +0100

    Moving to jung (yes, that should not be hardcoded)

--- start-vms.sh
+++ start-vms.sh
@@ -5,8 +5,8 @@ mkdir /var/run/twopence
 chown qemu:qemu /var/run/twopence
 chmod g+w /var/run/twopence
 
-virsh start almond04-build
-virsh start almond05-reference
-virsh start almond06-sut
+virsh start jung04-build
+virsh start jung05-reference
+virsh start jung06-sut
 
 chmod g+w /var/run/twopence/*
