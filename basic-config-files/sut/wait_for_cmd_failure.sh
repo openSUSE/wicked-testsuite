62230fb8295f2f21ea2998bc1ca496e4b51ed342
Bischoff <ebischoff@suse.com>
2015-03-13 12:24:33 +0100

    Finer-grained wait

--- basic-config-files/sut/wait_for_cmd_failure.sh
+++ basic-config-files/sut/wait_for_cmd_failure.sh
@@ -3,13 +3,13 @@
 # Parameters: "<shell-command>"
 # Returns 0 if failed in less than 30 seconds
 
-for ((i = 0; i < 30; i++)); do
+for ((i = 0; i < 300; i++)); do
   echo $@ | /bin/bash
   if [ $? -ne 0 ]; then
     echo "Command \"$@\" failed"
     exit 0
   fi
-  sleep 1
+  usleep 100000
 done
 
 echo "Giving up."
--- basic-config-files/sut/wait_for_cmd_success.sh
+++ basic-config-files/sut/wait_for_cmd_success.sh
@@ -3,13 +3,13 @@
 # Parameters: "<shell-command>"
 # Returns 0 if succeeded in less than 30 seconds
 
-for ((i = 0; i < 30; i++)); do
+for ((i = 0; i < 300; i++)); do
   echo $@ | /bin/bash
   if [ $? -eq 0 ]; then
     echo "Command \"$@\" succeeded"
     exit 0
   fi
-  sleep 1
+  usleep 100000
 done
 
 echo "Giving up."
