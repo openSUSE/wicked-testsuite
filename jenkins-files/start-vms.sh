ab4171d9c2d37ba4a9a6abe24cc767d07fb1c7da
Bischoff <ebischoff@suse.com>
2015-04-14 11:36:50 +0200

    Added helper script for manual runs

---
+++ jenkins-files/start-vms.sh
@@ -0,0 +1,74 @@
+#! /bin/bash
+#
+# Start the VMs for manual testing
+#
+# Assumes that wicked has already been build for that project
+
+name=$1
+id=$2
+
+# Parameters
+
+ref=openSUSE_13_1-x86_64.qcow2
+ref_arch=x86_64
+
+sut=openSUSE_Tumbleweed-x86_64.qcow2
+sut_arch=x86_64
+
+nanny=with
+
+# Start the VMs and networks
+
+scripts=$(dirname $0)
+
+images=/var/lib/libvirt/images
+jenkins=/var/lib/jenkins
+
+export WORKSPACE=$jenkins/jobs/$name/workspace
+
+rm -f $WORKSPACE/*.qcow2
+cp $images/ref/$ref $WORKSPACE/ref.qcow2
+cp $images/sut/$sut $WORKSPACE/sut.qcow2
+
+$scripts/config-net.sh $name $id 0
+$scripts/config-net.sh $name $(($id+50)) 1
+$scripts/config-ref.sh $name $id $ref_arch
+$scripts/config-sut.sh $name $id $sut_arch
+
+## Wait until we can communicate with the test machines
+
+target_sut=virtio:/var/run/twopence/sut-$name.sock
+target_ref=virtio:/var/run/twopence/ref-$name.sock
+
+while true; do
+  who=$(twopence_command -b $target_ref "whoami")
+  [ "$who" = "root" ] && break
+  sleep 1
+done
+
+while true; do
+  who=$(twopence_command -b $target_sut "whoami")
+  [ "$who" = "root" ] && break
+  sleep 1
+done
+
+# Inject wicked
+
+for pkg in $(ls $WORKSPACE/RPMs); do
+  twopence_inject $target_sut $WORKSPACE/RPMs/$pkg /root/$pkg
+done
+
+twopence_command $target_sut "service wicked stop"
+twopence_command $target_sut "service wickedd stop"
+
+twopence_command $target_sut "rpm -qc wicked > /tmp/config-files"
+twopence_command $target_sut "rpm -e --nodeps \$(rpm -qa \"*wicked*\")"
+twopence_command $target_sut "rm -f \$(cat /tmp/config-files)"
+twopence_command $target_sut "rpm -ih /root/*wicked*.$sut_arch.rpm"
+
+twopence_inject $target_sut $jenkins/cucumber/jenkins-files/wicked-$nanny-nanny.xml /etc/wicked/local.xml
+twopence_command $target_sut "chmod u=rw,go=r /etc/wicked/local.xml"
+
+twopence_command $target_sut "service wickedd start"
+twopence_command $target_sut "service wicked start"
+
