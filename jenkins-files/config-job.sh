a19c2b04955e8c166cf5228826a6c213486463a6
Marius Tomaschewski <mt@suse.de>
2015-06-12 16:27:08 +0200

    config-job.sh: create jobdir if missed

--- jenkins-files/config-job.sh
+++ jenkins-files/config-job.sh
@@ -2,15 +2,17 @@
 
 SUBDIR="cucumber"
 GIT_REPO="openSUSE"
+JOBS_DIR=/var/lib/jenkins/jobs
 
 function configure()
 {
+  test -d "$JOBS_DIR/$NAME" || mkdir -p "$JOBS_DIR/$NAME" || exit 1
   sed "s!@@SUBDIR@@!$SUBDIR!g;
        s!@@GIT_REPO@@!$GIT_REPO!g;
        s!@@BRANCH_NAME@@!$BRANCH_NAME!g;
        s!@@ID@@!$ID!g;
        s!@@NANNY@@!$NANNY!g" \
-      config-job.template > /var/lib/jenkins/jobs/$NAME/config.xml
+      config-job.template > "$JOBS_DIR/$NAME/config.xml"
 }
 
 BRANCH_NAME="master"
