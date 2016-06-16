908e5b822f231707fb3aa7472c7913ac7db50072
jloeser <jloeser@suse.de>
2016-06-16 16:30:21 +0200

    setup: improve dnsmasq/radvd deployment

--- host-config/setup_dnsmasq_radvd.sh
+++ host-config/setup_dnsmasq_radvd.sh
@@ -7,24 +7,37 @@ sh ./check_for_root.sh || exit 1
 source ./include.sh || exit 1
 
 #-----------------------------------------------------------------------------
-# dnsmasq and radvd
+# dnsmasq
 #-----------------------------------------------------------------------------
 echo " * configure dnsmasq..."
-if test -f "/etc/dnsmasq.conf" ; then
-    echo "WARNING: file '/etc/dnsmasq.conf' exists. Skip."
-else
-    cp ./files-host/dnsmasq.conf /etc
-    systemctl -q is-enabled dnsmasq || systemctl enable dnsmasq
-    systemctl -q is-active dnsmasq || systemctl start dnsmasq
+
+found="n"
+for i in /etc/dnsmasq.conf /etc/dnsmasq.d/*.conf ; do
+    grep -qs "^interface=wickedbr0" $i && found="y";
+done
+
+if [ "$found" = "n" ]; then
+    test -d /etc/dnsmasq.d || mkdir /etc/dnsmasq.d
+    cp -b ./files-host/dnsmasq.conf /etc/dnsmasq.d/wicked-testsuite.conf
+    if ! grep -qs "^conf-dir=/etc/dnsmasq.d" /etc/dnsmasq.conf; then
+        echo "conf-dir=/etc/dnsmasq.d" >> /etc/dnsmasq.conf
+    fi
 fi
 
+systemctl -q is-enabled dnsmasq || systemctl enable dnsmasq
+systemctl -q is-active dnsmasq || systemctl start dnsmasq
+
+#-----------------------------------------------------------------------------
+# radvd
+#-----------------------------------------------------------------------------
 echo " * configure radvd..."
-if test -f "/etc/radvd.conf" ; then
-    echo "WARNING: file '/etc/radvd.conf' exists. Skip."
-else
-    cp ./files-host/radvd.conf /etc
-    systemctl -q is-enabled radvd || systemctl enable radvd
-    systemctl -q is-active radvd || systemctl start radvd
+
+if ! grep -qs "^interface[ \t]*wickedbr" /etc/radvd.conf ; then
+    cp -b /etc/radvd.conf /etc/radvd.conf.backup
+    cp ./files-host/radvd.conf /etc/radvd.conf
 fi
+
+systemctl -q is-enabled radvd || systemctl enable radvd
+systemctl -q is-active radvd || systemctl start radvd
 
 exit 0
